4b9d2a9932715640245b8ad3b8960051ab54d8d7
Azimkhon1 <56162683+Azimkhon1@users.noreply.github.com>
2020-11-06 03:45:42 -0500

    Update settings.py

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -134,6 +134,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'unisalecustomer@gmail.com'
-EMAIL_HOST_PASSWORD = '87229200p'
+EMAIL_HOST_USER = 'EMAIL_LOGIN'
+EMAIL_HOST_PASSWORD = 'EMAIL_PASSWORD'
 
